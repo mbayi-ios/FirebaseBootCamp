685e6a84d36b638b5a168365925e565b641bea03
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-26 23:31:41 +0300

    firebase configured using app delegates

--- FirebaseBootCamp/FirebaseBootCampApp.swift
+++ FirebaseBootCamp/FirebaseBootCampApp.swift
@@ -10,13 +10,25 @@ import Firebase
 
 @main
 struct FirebaseBootCampApp: App {
-    init() {
-        FirebaseApp.configure()
-        print("firebase configured")
-    }
+//    init() {
+//        FirebaseApp.configure()
+//        print("firebase configured")
+//    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      print("firebase configured using app delegates")
+    return true
+  }
 }
