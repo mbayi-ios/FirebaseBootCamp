0402937cee575e3e79ca23fd004523d90e72549f
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-26 23:29:26 +0300

    firebase configured

--- FirebaseBootCamp/FirebaseBootCampApp.swift
+++ FirebaseBootCamp/FirebaseBootCampApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirebaseBootCampApp: App {
+    init() {
+        FirebaseApp.configure()
+        print("firebase configured")
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
