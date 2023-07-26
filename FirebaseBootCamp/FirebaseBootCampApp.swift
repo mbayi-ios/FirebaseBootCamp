1c1b56953b90cc55103f913d0fba4a8dd3197b92
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-26 23:19:07 +0300

    Initial Commit

---
+++ FirebaseBootCamp/FirebaseBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootCampApp.swift
+//  FirebaseBootCamp
+//
+//  Created by Ambrose Mbayi on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
